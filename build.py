bb414f765858e3fa66b9381773422216bffb5179
Zuki <530276014@qq.com>
2020-07-04 00:00:17 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,15 +1,22 @@
 from douyu import get_real_url as douyu
+from huya import get_real_url as huya
 
 OUTPUT_PATH = "C:\\Users\\sanch\\Desktop\\"
 NAME = "live"
 
 urls = {
-    "douyu": douyu
+    "douyu": douyu,
+    "huya": huya
 }
 
 slist = {
     "douyu": {
-        "贝拉": "3276456"
+        "贝拉": "3276456",
+        "学姐": "4775361"
+    },
+    "huya": {
+        "轩子":"xuanzi",
+        "恩七":"en777"
     }
 }
 
@@ -18,6 +25,13 @@ fl.write("#EXTM3U\n")
 
 for platform in slist:
     for streamer in slist[platform]:
-        fl.write("#EXTINF:-1,"+streamer)
-        fl.write("\n"+urls[platform](slist[platform][streamer])+"\n")
+        url = urls[platform](slist[platform][streamer])
 
+        #huya 
+        if isinstance(url,list):
+            url = url[0]
+        
+        if isinstance(url,str) and url.find("未开播")!=-1:
+            continue
+        fl.write("#EXTINF:-1,"+streamer)
+        fl.write(("\n"+url)+"\n")
